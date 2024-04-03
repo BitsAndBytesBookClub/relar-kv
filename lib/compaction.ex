34aa12679efa432418c466e372236c6fabd16872
relar <relardev@gmail.com>
2024-04-03 05:12:27 +0200

    finish compaction

--- lib/compaction.ex
+++ lib/compaction.ex
@@ -11,6 +11,7 @@ defmodule Kvstore.CompactionG do
 
   @ssts_path "db/sst"
   @level0_path "db/lsm/0"
+  @new_level0_path "db/compacted/0"
   @max_ssts 4
 
   def start_link(args) do
@@ -33,6 +34,9 @@ defmodule Kvstore.CompactionG do
     else
       Logger.info("Compacting SSTables")
       compact_ssts_into_level0()
+
+      # TODO move new level 0 to level 0
+
       {:noreply, %{state | ssts: 0}}
     end
   end
@@ -40,33 +44,34 @@ defmodule Kvstore.CompactionG do
   def compact_ssts_into_level0 do
     sst_data = get_sst_data()
     lsm_data = get_lsm_data()
+    write_data = get_writ_data()
 
-    combine_sst_and_lsm_keys(sst_data, lsm_data)
+    combine_sst_and_lsm_keys(sst_data, lsm_data, write_data)
   end
 
-  defp combine_sst_and_lsm_keys(sst_data, lsm_data) do
-    combine(sst_data, lsm_data, :both, nil, nil)
+  defp combine_sst_and_lsm_keys(sst_data, lsm_data, write_data) do
+    combine(write_data, sst_data, lsm_data, :both, nil, nil)
   end
 
-  defp combine([], [], _, _, _) do
+  defp combine(_, [], [], _, _, _) do
     Logger.info("Finished compacting SSTables")
   end
 
-  defp combine([], lsm_data, _, key, value) do
+  defp combine(write_data, [], lsm_data, _, key, value) do
     Logger.info("Writing remaining lsm data to LSM Level 0, key: #{key}, value: #{value}")
-    write_to_level0(key, value)
+    write_data = write_to_level0(write_data, key, value)
     {data, new_key, new_value} = next_from_lsm(lsm_data)
-    combine([], data, :lsm, new_key, new_value)
+    combine(write_data, [], data, :lsm, new_key, new_value)
   end
 
-  defp combine(sst_data, [], _, _, _) do
+  defp combine(write_data, sst_data, [], _, _, _) do
     {data, key, value} = next_from_sst(sst_data)
     Logger.info("Writing remaining sst data to LSM Level 0, key: #{key}, value: #{value}")
-    write_to_level0(key, value)
-    combine(data, [], :sst, nil, nil)
+    write_data = write_to_level0(write_data, key, value)
+    combine(write_data, data, [], :sst, nil, nil)
   end
 
-  defp combine(sst_data, lsm_data, pull, prev_key, prev_val) do
+  defp combine(write_data, sst_data, lsm_data, pull, prev_key, prev_val) do
     {{sst_data, sst_key, sst_value}, {lsm_data, lsm_key, lsm_value}} =
       case pull do
         :sst ->
@@ -82,23 +87,34 @@ defmodule Kvstore.CompactionG do
     case {sst_key, lsm_key} do
       {key, key} ->
         Logger.info("Keys are equal, writing sst key: #{sst_key} to LSM Level 0")
-        write_to_level0(key, sst_value)
-        combine(sst_data, lsm_data, :both, nil, nil)
+        write_data = write_to_level0(write_data, key, sst_value)
+        combine(write_data, sst_data, lsm_data, :both, nil, nil)
 
       {s, l} when s < l ->
         Logger.info("Writing sst key: #{sst_key} to LSM Level 0")
-        write_to_level0(sst_key, sst_value)
-        combine(sst_data, lsm_data, :sst, lsm_key, lsm_value)
+        write_data = write_to_level0(write_data, sst_key, sst_value)
+        combine(write_data, sst_data, lsm_data, :sst, lsm_key, lsm_value)
 
       {s, l} when s > l ->
         Logger.info("Writing lsm key: #{lsm_key} to LSM Level 0")
-        write_to_level0(lsm_key, lsm_value)
-        combine(sst_data, lsm_data, :lsm, sst_key, sst_value)
+        write_data = write_to_level0(write_data, lsm_key, lsm_value)
+        combine(write_data, sst_data, lsm_data, :lsm, sst_key, sst_value)
     end
   end
 
-  defp write_to_level0(key, value) do
-    IO.puts("[[[#{inspect(key)},#{inspect(value)}]]]")
+  defp write_to_level0({fd, count, letter}, key, value) do
+    case count > 5 do
+      true ->
+        File.close(fd)
+        next_letter = NextLetter.get_next_letter(letter)
+        {:ok, fd} = File.open(@new_level0_path <> "/#{next_letter}", [:write, :utf8])
+        IO.write(fd, "#{key},#{value}")
+        {fd, 1, next_letter}
+
+      false ->
+        IO.write(fd, "#{key},#{value}")
+        {fd, count + 1, letter}
+    end
   end
 
   defp next_from_lsm([]) do
@@ -124,6 +140,13 @@ defmodule Kvstore.CompactionG do
         [key, value] = String.split(item, ",")
         {data, key, value}
     end
+  end
+
+  defp get_writ_data() do
+    File.mkdir_p!(@new_level0_path)
+
+    {:ok, fd} = File.open(@new_level0_path <> "/a", [:write, :utf8])
+    {fd, 0, "a"}
   end
 
   defp get_lsm_data() do
@@ -197,5 +220,13 @@ defmodule Kvstore.CompactionG do
     data = Enum.filter(data, fn x -> x != nil end)
 
     {data, key, value}
+  end
+end
+
+defmodule NextLetter do
+  def get_next_letter(letter) when byte_size(letter) == 1 do
+    [char_code] = String.to_charlist(letter)
+    next_char_code = char_code + 1
+    List.to_string([next_char_code])
   end
 end
