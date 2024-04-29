5073404a0f98eedb79cded5a782acea579f72703
relar <relardev@gmail.com>
2024-04-29 19:58:33 +0200

    undo debug

--- lib/broadcast.ex
+++ lib/broadcast.ex
@@ -1,11 +1,8 @@
 defmodule Kvstore.Broadcast do
   def broadcast(message) do
-    send =
-      case Application.get_env(:kvstore, :broadcast) do
-        nil -> {:error, "No broadcast configured"}
-        func -> func.(message)
-      end
-
-    dbg({message, send})
+    case Application.get_env(:kvstore, :broadcast) do
+      nil -> :ok
+      func -> func.(message)
+    end
   end
 end
